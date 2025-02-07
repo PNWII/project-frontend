e54f7ca56b771a2d16110cede3ab43ff71e5b64c
Panwasa Nimaim <panwasanimaim@gmail.com>
2025-02-07 19:30:57 +0700

    Initial commit for frontend

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,6 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom"],
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000,
-  },
+  plugins: [react()],
 });
