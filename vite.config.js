234da1be04c90a97bf75a72df245fa6ad70df864
Panwasa Nimaim <panwasanimaim@gmail.com>
2025-02-07 19:05:40 +0700

    Initial commit for frontend

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: "0.0.0.0", // ทำให้เซิร์ฟเวอร์สามารถเข้าถึงจากภายนอกได้
-    port: 4173, // ใช้พอร์ต 8080 โดยตรง
-    open: true, // เปิดหน้าเว็บในเบราว์เซอร์อัตโนมัติ
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
   },
 });
