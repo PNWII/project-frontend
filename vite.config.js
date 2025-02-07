f603be4308715b49d963b7959b2d1c40713b086a
Panwasa Nimaim <panwasanimaim@gmail.com>
2025-02-07 15:21:27 +0700

    Configure port and host for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',   // ทำให้แอปสามารถเข้าถึงได้จากภายนอก
+    port: 8080,         // กำหนดพอร์ตที่แอปจะรัน
+  },
 });
